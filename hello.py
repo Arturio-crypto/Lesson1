bf1d3f227f5608d07554f4de2cc30ee1ff56d7b1
Artur Predko <a0370ii1@gmail.com>
2022-04-12 23:13:14 +0300

    Удалили ненужную чать

--- hello.py
+++ hello.py
@@ -3,10 +3,3 @@ intro = "Here is important information about your account security."
 
 first_name = 'Joffrey'
 greeting = 'Hello'
-
-# BEGIN (write your solution here)
-head = (f'{greeting}, {first_name}!')
-# END
-print(head)
-body = (f'{intro}\n{info}')
-print(body)

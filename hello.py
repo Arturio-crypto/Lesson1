a93b473182a6de40b1c91573d84c5f4c0ad22734
Artur Predko <a0370ii1@gmail.com>
2022-04-13 17:41:37 +0300

    (ver2)

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
-name = input('Введите имя: ')
-age = int(input('Сколько Вам лет: '))
-name = 'Вас зовут: '+ name
-age = 2022 - age
-print(name)
-print(f'Вы родились в: {age} году!')
+#name = input('Введите имя: ')
+#age = int(input('Сколько Вам лет: '))
+#name = 'Вас зовут: '+ name
+#age = 2022 - age
+#print(name)
+#print(f'Вы родились в: {age} году!')
+
+a = input("What's your age? :")
+b = 100 - int(a)
+print('Вам осталось жить, примерно: ' + str(b))

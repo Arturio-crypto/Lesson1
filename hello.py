f9493440511310b757cf11b4883c76fbaf53ae25
Predko Artur <a0370ii1@gmail.com>
2022-04-28 03:02:28 +0300

    Тест

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 #name = input('Введите имя: ')
 #age = int(input('Сколько Вам лет: '))
 #name = 'Вас зовут: '+ name
-#age = 2022 - age
+#age = 2021 - age
 #print(name)
 #print(f'Вы родились в: {age} году!')
 
